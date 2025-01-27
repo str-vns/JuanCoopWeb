ce27f1436570b8eaac703db003f6607bfa296033
Flappy <stvnsbarrantes@gmail.com>
2025-01-28 01:30:48 +0800

    dwadwad

--- src/components/Address/addressCreate.jsx
+++ src/components/Address/addressCreate.jsx
@@ -4,7 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { reverseCode, forwardCode } from "@redux/Actions/locationActions";
 import { addAddress } from '@redux/Actions/addressActions';
 import { getCurrentUser, getToken } from "@utils/helpers";
-import "@assets/css/addresscreate.css";
+import "@assets/css/addressCreate";
 import Modal from "react-modal";
 import Map, { Marker } from "react-map-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
