af44eeebff96143195c15becd2a5b11ff852da59
Flappy <stvnsbarrantes@gmail.com>
2025-01-27 19:51:46 +0800

    add

--- src/components/Admin/rankProduct.jsx
+++ src/components/Admin/rankProduct.jsx
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom";
 import BarGraph from "../../assets/css/BarGraph";
 import Sidebar from "./sidebar";
 import { IoArrowBackOutline } from "react-icons/io5"; // Using react-icons for the back button
+import styles from "../../assets/css/rankProduct";
 
 const RankedProductsPage = () => {
   const dispatch = useDispatch();
@@ -42,42 +43,3 @@ const RankedProductsPage = () => {
 };
 
 export default RankedProductsPage;
-
-// Inline styles object
-const styles = {
-  container: {
-    padding: "16px",
-    backgroundColor: "#fff",
-    minHeight: "100vh",
-    fontFamily: "Arial, sans-serif",
-    width: "calc(100% - 400px)", // Corrected the syntax for `calc`
-    marginLeft: "300px", // Corrected the syntax for `margin-left`
-  },
-  header: {
-    display: "flex",
-    alignItems: "center",
-    marginBottom: "16px",
-  },
-  backButton: {
-    backgroundColor: "transparent",
-    border: "none",
-    cursor: "pointer",
-    padding: "8px",
-    marginRight: "10px",
-  },
-  headerTitle: {
-    fontSize: "24px",
-    fontWeight: "bold",
-    alignItems: "center",
-  },
-  loader: {
-    fontSize: "18px",
-    textAlign: "center",
-    margin: "20px 0",
-  },
-  error: {
-    color: "red",
-    fontSize: "16px",
-    textAlign: "center",
-  },
-};
